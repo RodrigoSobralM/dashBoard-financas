23652b825c365b4ee40f6ec87081f074ba95a7cc
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2025-03-11 09:10:52 -0300

    :sparkles: feat: MaterialUI insert

---
+++ src/components/BannerImage.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const BannerImage = styled.div`
+    background-image: url(/illustration-authentication.svg);
+    background-size: cover;
+    height: 100vh;
+    border-radius: 12px;
+`
